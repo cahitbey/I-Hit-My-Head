160afddcfd881c211966b8717212818498900907
Cihat Kızılkaya <cahitbeychief@gmail.com>
2022-07-29 16:43:10 +0300

    added wall, get it to break

---
+++ Assets/Scripts/BreakWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWall : MonoBehaviour
+{
+    public GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hitpoint olacak, mouse left clik değil de hit point 0a düşünce kırılacak.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            wall.GetComponent<MeshRenderer>().enabled = false;
+            wall.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
+ 
